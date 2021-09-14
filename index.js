4d1520c390302676d0f94a63fd873435be8a7eb1
yatsun <yatsunlam25@gmail.com>
2021-09-13 23:45:15 -0400

    commit tree 0daa22fac412289d3cce65a787429de3bb76dd54

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function saturdayFun(toDo="roller-skate"){
+    return `This Saturday, I want to ${toDo}!`;
+}
+
+
+const mondayWork = function(mondayTask="go to the office") {
+    return `This Monday, I will ${mondayTask}.`;
+}
+
+let wrapAdjective = function(defaultValue="*"){
+    return function(nothing="special") {
+        return `You are ${defaultValue}${nothing}${defaultValue}!`
+    }
+}
+
+const Calculator = {
+    add: function(a,b){
+        return a + b;
+    },
+    subtract: function(a,b){
+        return a - b;
+    },
+    multiply: function(a,b){
+        return a*b;
+    },
+    divide: function(a,b){
+        return a/b;
+    }
+}
+
+let actionApplyer = function(a, arr){
+    let start = a
+
+    for (let i = 0; i < arr.length; i++){
+        start = arr[i](start)
+    }
+    return start
+}
+
